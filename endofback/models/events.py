dc374266220a93567e1697f3bb56c3df0cdb8797
Maitai <itsjustmaitai@gmail.com>
2026-05-06 19:50:38 +0300

    Fix :Added the field 'None' to avoid validation error

--- endofback/models/events.py
+++ endofback/models/events.py
@@ -6,12 +6,12 @@ from sqlalchemy import Column, ARRAY, TEXT
 class Sherehe(SQLModel , table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    description: Optional[str]
+    description: Optional[str] = Field(default=None)
     date: datetime
     location: str
     activities: Optional[List[str]] = Field(default=None, sa_column=Column(ARRAY(TEXT)), default=None)
     price: Optional[float]
-    public_rating: Optional[float]
-    sku:Optional[str]
+    public_rating: Optional[float] = Field(default=None)
+    sku:Optional[str] = Field(default=None)
     image_url: Optional[str] = Field(default=None)
  
