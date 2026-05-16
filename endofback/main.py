cefaf8cbb153da9b34e78703f7f34fe028fee988
Maitai <itsjustmaitai@gmail.com>
2026-05-16 14:51:59 +0300

    removed comment

--- endofback/main.py
+++ endofback/main.py
@@ -51,8 +51,6 @@ app.add_middleware(
 async def add_security_headers(request: Request, call_next):
     start_time = time.time()
     response = await call_next(request)
-    
-    # Security Headers
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
