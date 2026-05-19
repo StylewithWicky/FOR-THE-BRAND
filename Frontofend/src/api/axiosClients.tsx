fe7138cc8b0d214e3c554bf1946f3a2a18d0d51d
Maitai <itsjustmaitai@gmail.com>
2026-05-19 17:59:37 +0300

    Fix:Ensures the api_url is calm

--- Frontofend/src/api/axiosClients.tsx
+++ Frontofend/src/api/axiosClients.tsx
@@ -15,7 +15,7 @@ axiosClient.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
     const token = localStorage.getItem('token');
     if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
