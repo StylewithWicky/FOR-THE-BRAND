6ecd1651817d668546f3a7c2f3132db03abd8c47
Maitai <itsjustmaitai@gmail.com>
2026-05-19 18:07:17 +0300

    Fix : Chaned the admin from string to a dict

--- endofback/routes/msee.py
+++ endofback/routes/msee.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session, select
 from datetime import timedelta, datetime
 import os
+import json
 from dotenv import load_dotenv, find_dotenv
 
 from models.msee import Mzee
@@ -11,18 +12,14 @@ from auth.database import get_session
 from schema.msee import MzeeCreate
 from pydantic import BaseModel
 
-# 1. Initialize logic
 load_dotenv(find_dotenv())
 router = APIRouter()
-
-# 2. Get Admin Emails from .env
-# Using .get() with a default empty string avoids crashes if the key is missing
-ADMIN_EMAILS = os.getenv("ADMIN_EMAIL", "").split(",") 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
     role: str
+    is_admin: bool
     email: str 
 
 @router.post("/auth/register", status_code=status.HTTP_201_CREATED)
@@ -46,16 +43,15 @@ def register_mzee(user_in: MzeeCreate, session: Session = Depends(get_session)):
     return {"message": "Mzee registered successfully"}
 
 
+
 @router.post("/login", response_model=Token)
 def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), 
     session: Session = Depends(get_session)
 ):
-    # 1. Look for the user
     statement = select(Mzee).where(Mzee.email == form_data.username)
     user_record = session.exec(statement).first() 
-    
-    # 2. Check credentials
+
     if not user_record or not verify_password(form_data.password, user_record.hashed_password):
         print(f"SECURITY_ALERT: Failed login attempt for {form_data.username}")
         raise HTTPException(
@@ -64,18 +60,21 @@ def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"}
         )
     
-   
-    is_admin = user_record.email in ADMIN_EMAILS
+    try:
+        raw_admin_emails = json.loads(os.getenv("ADMIN_EMAIL", "[]"))
+        admin_emails = [email.strip().lower() for email in raw_admin_emails]
+    except Exception:
+        admin_emails = []
+
+    is_admin = user_record.email.lower() in admin_emails
     user_role = "admin" if is_admin else "user"
 
-    # LOGGING THE ENTRY (Believe !!)
     print(f"--- TERMINAL_ENTRY_LOG ---")
     print(f"IDENT: {user_record.email}")
     print(f"TIME:  {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     print(f"ROLE:  {user_role}")
     print(f"--------------------------")
 
-    # 4. Create the security token
     access_token_expires = timedelta(minutes=30)
     access_token = create_access_token(
         data={"sub": user_record.email, "role": user_role}, 
@@ -86,5 +85,6 @@ def login_for_access_token(
         "access_token": access_token,
         "token_type": "bearer",
         "role": user_role,
+        "is_admin": is_admin,
         "email": user_record.email
     }
