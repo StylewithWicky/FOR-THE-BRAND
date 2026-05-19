2546d55feb047c22bc68bd909e3812c8a3b5c05b
Maitai <itsjustmaitai@gmail.com>
2026-05-19 18:05:27 +0300

    Fix :Nothing major changed the name from passkey to passcode

--- endofback/config.py
+++ endofback/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class ServerSettings(BaseSettings):
     redis_url: str = "redis://localhost:6379"
@@ -6,7 +7,8 @@ class ServerSettings(BaseSettings):
     mpesa_consumer_key: str
     mpesa_consumer_secret: str
     mpesa_shortcode: str
-    mpesa_password: str
+    mpesa_passcode: str  
+    safaricom_subnets: List[str] = ["196.201.214.0/24", "196.201.213.0/24"]
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
